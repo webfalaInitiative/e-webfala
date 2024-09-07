41f8f6d8c26157227904095ee56b3c89b0d035a8
DevKaffy <kafilatadewumi61@gmail.com>
2024-09-07 08:53:21 +0100

    moved updates from main branch to payments branch

--- e_webfala/settings.py
+++ e_webfala/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'e_webfala.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
