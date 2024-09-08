3857fc95b7a54a8cc41e878c352124402e29dc01
papycoda <opeyemi655@gmail.com>
2024-09-08 14:05:28 +0100

    "Removed template files and updated views to render templates without directory prefix"

--- payments/views.py
+++ payments/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def payments(request):
-    return render(request, 'payments/payments.html') 
+    return render(request, 'payments.html') 
