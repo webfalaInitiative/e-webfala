874c163f09db880c29e881de74a37ad6197a7944
HoluwatobiHarbolanle <bolanleoluwatobi844@gmail.com>
2024-09-09 14:09:24 -0700

    Set up Postgresql database

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=30, unique=True, blank=False)
+    email = models.EmailField(max_length=30, unique=True, blank=False)
+    
--- e_webfala/settings.py
+++ e_webfala/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'e_webfala.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'user_db',
+        'USER': 'group_1_user',
+        'PASSWORD': 'tobi24#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
